f7c3c379959cfeaaf140141d8132b6c3e730dbd0
me--2014 <a.ruffle.2014@my.bristol.ac.uk>
2016-07-28 09:37:48 +0100

    Added further tests - for line breaks in story text and headings in tag responses

--- blog/tests.py
+++ blog/tests.py
@@ -1,16 +1,18 @@
 """
 Test suite for blog application. To run use command 'python manage.py test blog'.
-Note: SimpleTestCase contains useful additional assert methods.
 """
 
 from django.test import TestCase, SimpleTestCase
 from .models import Tag, Story, User, Favourite
 from datetime import date
-import json, collections
+import json
+import collections
 from urllib import quote
 from rest_framework.parsers import JSONParser
 import datetime
 from operator import itemgetter
+
+# TO DO: f these are only used by stories class it may be sensible to move them within that class
 
 encoded_string_first = quote("first")
 encoded_string_story = quote("story")
@@ -26,7 +28,8 @@ query_strings_valid_empty = ['user_id=2', 'tag_id=7', 'title_text=' + encoded_st
 query_strings_user = ['user_id=1', 'user_id=1&tag_id=2', 'user_id=1&title_text=' + encoded_string_first,
                       'user_id=1&tag_id=2&title_text=' + encoded_string_story]
 
-class checkStories(TestCase):
+
+class CheckStories(TestCase):
     def setUp(self):
         tag1 = Tag.objects.create(id=1, name="short")
         tag1.save()
@@ -35,13 +38,16 @@ class checkStories(TestCase):
         tag3 = Tag.objects.create(id=3, name="romantic")
         tag3.save()
 
-        story1 = Story.objects.create(id=1, title="My first story", storytext="Once upon a time", dateposted="2010-12-19")
+        story1 = Story.objects.create(id=1, title="My first story", storytext="Once upon a time",
+                                      dateposted="2010-12-19")
         story1.save()
         story1.tags.add(tag1, tag3)
-        story2 = Story.objects.create(id=2, title="My second story", storytext="Happy ever after", dateposted="2010-01-30")
+        story2 = Story.objects.create(id=2, title="My second story", storytext="Happy ever after",
+                                      dateposted="2010-01-30")
         story2.save()
         story2.tags.add(tag2, tag3)
-        story3 = Story.objects.create(id=3, title="My third story", storytext="Three brown mice", dateposted="2010-01-30")
+        story3 = Story.objects.create(id=3, title="My third story", storytext="Three brown mice\nSee how they run",
+                                      dateposted="2010-01-30")
         story3.save()
         story3.tags.add(tag2)
 
@@ -50,7 +56,6 @@ class checkStories(TestCase):
 
         fav1 = Favourite.objects.create(id=1, story=story3, user=user1)
         fav1.save()
-
 
     def test_json_response_provided(self):
         """
@@ -66,7 +71,7 @@ class checkStories(TestCase):
             filled = True
         self.assertTrue(filled)
 
-        url = url + '?'
+        url += '?'
         json_response = self.client.get(url)
         self.assertIsNotNone(json_response)
         filled = False
@@ -83,7 +88,7 @@ class checkStories(TestCase):
                 filled = True
             self.assertTrue(filled)
 
-        # Check query strings which should return an empty JSON response
+        #  Check query strings which should return an empty JSON response
         url = '/rest-api/stories/'
         for item in query_strings_valid_empty:
             current_url = url + item
@@ -94,15 +99,15 @@ class checkStories(TestCase):
                 empty = True
             self.assertTrue(empty)
 
-        # Check invalid query strings
+        #  Check invalid query strings
 
         json_response = self.client.get('rest-api/stories/tag_id=2')  # Missing the '?' from start of querystring
         self.assertEqual(json_response.status_code, 404)
 
         with self.assertRaises(ValueError):
-            self.client.get('/rest-api/stories/?tag_id=1title_text=hello') # Missing the '&' between parameters
+            self.client.get('/rest-api/stories/?tag_id=1title_text=hello')  # Missing the '&' between parameters
 
-        json_response = self.client.get('rest-api/stories/uesr_id=1') # Parameter name is misspelled
+        json_response = self.client.get('rest-api/stories/uesr_id=1')  # Parameter name is misspelled
         self.assertEqual(json_response.status_code, 404)
 
     def test_response_contains_correct_headings(self):
@@ -110,7 +115,7 @@ class checkStories(TestCase):
         Check that stories sent in the response contain headings such as id, title, dateposted, etc
         """
 
-        # Check that Story model fields are included in the response
+        #  Check that Story model fields are included in the response
         url = '/rest-api/stories/'
         json_response = self.client.get(url)
         self.assertContains(json_response, 'id')
@@ -121,7 +126,7 @@ class checkStories(TestCase):
         self.assertContains(json_response, 'wordcount')
         self.assertContains(json_response, 'author')
         self.assertContains(json_response, 'tags')
-        url = url + '?'
+        url += '?'
         for item in query_strings_valid_filled:
             current_url = url + item
             json_response = self.client.get(current_url)
@@ -168,20 +173,18 @@ class checkStories(TestCase):
         json_response = self.client.get(url)
         story_dates = []
 
+        # Get dateposted and title from each story
         for story in json_response.data:
             date_info = story['dateposted'].split('-')
-            date = datetime.date(int(date_info[0]), int(date_info[1]), int(date_info[2]))
-            title = story['title']
-            story_dates.append((date, title))
+            story_date = datetime.date(int(date_info[0]), int(date_info[1]), int(date_info[2]))
+            story_title = story['title']
+            story_dates.append((story_date, story_title))
 
-        print("Before sort:")
-        print(unicode(story_dates))
-
+        # Create sorted version
         story_dates_ordered = sorted(story_dates, key=itemgetter(0), reverse=True)
         story_dates_ordered = sorted(story_dates_ordered, key=itemgetter(1))
-        print("After sort:")
-        print(unicode(story_dates_ordered))
 
+        # Compare original and sorted versions, which should be the same
         i = 0
         failures = 0
         for item in story_dates:
@@ -191,43 +194,16 @@ class checkStories(TestCase):
 
         self.assertEqual(failures, 0)
 
+    def test_line_breaks(self):
+        """
+        Check that stories with line breaks contain '\n'
+        """
+        url = '/rest-api/stories/3/'
+        json_response = self.client.get(url)
+        self.assertContains(json_response, '\\n')
 
 
-"""
-class GeneralTests(TestCase):
-
-    def setUp(self):
-        tag1 = Tag.objects.create(id=1, name="short")
-        tag1.save()
-        tag2 = Tag.objects.create(id=2, name="long")
-        tag2.save()
-        tag3 = Tag.objects.create(id=3, name="romantic")
-        tag3.save()
-        story1 = Story.objects.create(id=1, title="My first story", storytext="Once upon a time")
-        story1.save()
-        story1.tags.add(tag1, tag3)
-        story2 = Story.objects.create(id=2, title="My second story", storytext="Happy ever after")
-        story2.save()
-        story2.tags.add(tag2, tag3)
-        story3 = Story.objects.create(id=3, title="My third story", storytext="Three brown mice")
-        story3.save()
-        story3.tags.add(tag2)
-        user1 = User.objects.create(id=1, name="anon")
-        user1.save()
-
-    def test_header_on_every_page(self):
-
-        discover = self.client.get('/')
-        contact = self.client.get('/contact/')
-        about = self.client.get('/about/')
-        test_pages = [discover, contact, about]
-        fails = 0
-        for page in test_pages:
-            if 'header_top' not in page.content:
-                fails += 1
-        self.assertEqual(fails, 0)
-
-class DiscoverTests(TestCase):
+class CheckTags(TestCase):
 
     def setUp(self):
         tag1 = Tag.objects.create(id=1, name="short")
@@ -236,65 +212,41 @@ class DiscoverTests(TestCase):
         tag2.save()
         tag3 = Tag.objects.create(id=3, name="romantic")
         tag3.save()
-        story1 = Story.objects.create(id=1, title="My first story", storytext="Once upon a time", dateposted="2000-01-01")
+
+        story1 = Story.objects.create(id=1, title="My first story", storytext="Once upon a time",
+                                      dateposted="2010-12-19")
         story1.save()
         story1.tags.add(tag1, tag3)
-        story2 = Story.objects.create(id=2, title="My second story", storytext="Happy ever after", dateposted="2005-10-13")
+        story2 = Story.objects.create(id=2, title="My second story", storytext="Happy ever after",
+                                      dateposted="2010-01-30")
         story2.save()
         story2.tags.add(tag2, tag3)
-        story3 = Story.objects.create(id=3, title="My third story", storytext="Three brown mice", dateposted="1992-05-31")
+        story3 = Story.objects.create(id=3, title="My third story", storytext="Three brown mice\nSee how they run",
+                                      dateposted="2010-01-30")
         story3.save()
         story3.tags.add(tag2)
+
         user1 = User.objects.create(id=1, name="anon")
         user1.save()
 
-    def test_order_stories(self):
+        fav1 = Favourite.objects.create(id=1, story=story3, user=user1)
+        fav1.save()
 
-        response = self.client.get('/0/getTaggedStories/', HTTP_X_REQUESTED_WITH='XMLHttpRequest')
-        stories = json.loads(response.content, object_pairs_hook=collections.OrderedDict)
-        fails = 0
-        prev_date = ""
-        for story in stories:
-                if prev_date != "":
-                    current_date = stories[story]['dateposted']
-                    if prev_date < current_date:
-                        fails+= 1
-                prev_date = stories[story]['dateposted']
-        self.assertEqual(fails, 0)
+    def test_response_contains_correct_headings(self):
+        """
+        Check that tags sent in the response contain headings such as 'id'
+        """
 
-    def test_tag_filter_chooses_correct_stories(self):
+        urls = ['/rest-api/tags/', '/rest-api/tags/?', '/rest-api/tags/2/', ]
 
+        for url in urls:
+            json_response = self.client.get(url)
+            self.assertContains(json_response, 'id')
+            self.assertContains(json_response, 'name')
 
-        no_stories_message = "There are no stories with that tag."
-        fails = 0
-        all_tags = Tag.objects.all()
-        for tag in all_tags:
-            response = self.client.get('/' + unicode(tag.id) + '/getTaggedStories/',
-                                       HTTP_X_REQUESTED_WITH='XMLHttpRequest')
-            stories_with_tag = Story.objects.filter(tags__id=tag.id)
-            if len(stories_with_tag) > 0:
-                for story in stories_with_tag:
-                    if story.title not in response.content:
-                        fails += 1
-            elif len(stories_with_tag) == 0:
-                if no_stories_message not in response:
-                    fails += 1
-            else:
-                fails += 1
-        response = self.client.get(
-            '/' + unicode(0) + '/getTaggedStories/',
-            HTTP_X_REQUESTED_WITH='XMLHttpRequest')  # Filter with value 0 should result in all stories
-        all_stories = Story.objects.all()
-        if len(all_stories) > 0:
-            for story in all_stories:
-                if story.title not in response.content:
-                    fails += 1
-        elif len(all_stories) == 0:
-            if no_stories_message not in response:
-                fails += 1
-        else:
-            fails += 1
+    def test_all_ids_unique(self):
+        """
+        Check that there are no duplicate tag ids in the response
+        """
 
-        self.assertEqual(fails, 0)
-
-"""
+        # TO DO: add this test
